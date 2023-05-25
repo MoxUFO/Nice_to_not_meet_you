40719e61b2cbfe9f2ae5c969bd0a728eecbc2254
MOX UFO <mox.ufo@gmail.com>
2023-05-25 15:44:45 -0400

    edited seeds, started user routes and controllers

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const db = require('./config/connection');
-// const routes = require('./routes');
+const routes = require('./routes');
+const User = require('./models/User')
 
 
 
@@ -11,7 +12,22 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(routes);
+app.use(routes);
+
+// app.post('/post', (req,res)=>{
+//   const newUser = new User({
+//       username: req.body.username,
+//       email: req.body.email,
+//       thoughts:[],
+//       friends:[]
+//     })
+//     newUser.save();
+//     if (newUser) {
+//       res.status(201).json(newUser)
+//     } else {
+//       res.status(500).json({error: 'try again bozo'})
+//     }
+// })
 
 db.once('open', () => {
   app.listen(PORT, () => {
--- utils/seeds.js
+++ utils/seeds.js
@@ -6,16 +6,16 @@ connection.on('error', (err) => err);
 
 connection.once('open', async () => {
   console.log('connected');
-  await Thoughts.deleteMany({});
+//   await Thoughts.deleteMany({});
   await User.deleteMany({});
 
 
   await User.collection.insertMany(userData);
-  await Thoughts.collection.insertMany(thoughtsData);
+//   await Thoughts.collection.insertMany(thoughtsData);
 
   // loop through the saved videos, for each video we need to generate a video response and insert the video responses
   console.table(userData);
-  console.table(thoughtsData);
+//   console.table(thoughtsData);
   console.info('Seeding complete! 🌱');
   process.exit(0);
 });
